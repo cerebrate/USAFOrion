331e653863503f874659fd605749caf2a341fbcb
Alistair Young <avatar@arkane-systems.net>
2014-12-25 10:56:34 -0600

    ModuleWindow.

---
+++ USAFOrion/ModuleWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Tac ;
+
+namespace USAFOrion
+{
+    public abstract class ModuleWindow<T> : Window<T>
+    {
+        protected PartModule myPartModule ;
+
+        protected ModuleWindow (string windowTitle, PartModule p, float defaultWidth, float defaultHeight)
+            : base (windowTitle, defaultWidth, defaultHeight) { this.myPartModule = p ; }
+    }
+}
